e442204a3fe9a71849b848c066c6113912b27af4
Sune Askjaer <sune.askjaer@gmail.com>
2012-02-22 23:26:15 +0100

    Updated ConceptWiki uuids for general target and compound concepts"

--- app/controllers/concept_wiki_api_calls_controller.rb
+++ app/controllers/concept_wiki_api_calls_controller.rb
@@ -15,7 +15,7 @@ class ConceptWikiApiCallsController < ApplicationController
       options[:limit] =  params[:limit]
       options[:offset] = params[:offset]
       api_call = ConceptWikiApiCall.new
-      results = api_call.search_by_tag('7efb2a9c-f5e1-44c9-b5d4-3a364ce1bada',substring, options)  # this is the 'Pharmacologic Substance'Amino Acid, Peptide, or Protein' tag
+      results = api_call.search_by_tag('07a84994-e464-4bbf-812a-a4b96fa3d197',substring, options)  # this is the 'Pharmacologic Substance'Amino Acid, Peptide, or Protein' tag
       render :json => results.to_json, :layout => false
   end
   
@@ -24,7 +24,16 @@ class ConceptWikiApiCallsController < ApplicationController
       options[:limit] =  params[:limit]
       options[:offset] = params[:offset]
       api_call = ConceptWikiApiCall.new
-      results = api_call.search_by_tag('047a9f9a-5c5b-461e-a5ae-76ec68afda02',substring, options)  # this is the 'Amino Acid, Peptide, or Protein' tag
+      results = api_call.search_by_tag('eeaec894-d856-4106-9fa1-662b1dc6c6f1',substring, options)  # this is the 'Amino Acid, Peptide, or Protein' tag
+      render :json => results.to_json, :layout => false
+  end
+  
+  def non_species_specific_lookup(substring = params[:query])
+      options = Hash.new
+      options[:limit] =  params[:limit]
+      options[:offset] = params[:offset]
+      api_call = ConceptWikiApiCall.new
+      results = api_call.search_by_tag('504ea493-f757-4921-b699-3843fcacae1d',substring, options)  # this is the 'Amino Acid, Peptide, or Protein' tag
       render :json => results.to_json, :layout => false
   end
   
