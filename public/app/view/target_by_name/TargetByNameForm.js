28fc93f0bd93b18a464e6ca15c89cc9bc5f8aba5
James Eales <jeales@gmail.com>
2012-03-07 17:12:00 +0000

    Added loading of basic single target functionality, styling and layout still to come

--- public/app/view/target_by_name/TargetByNameForm.js
+++ public/app/view/target_by_name/TargetByNameForm.js
@@ -1,54 +1,59 @@
 Ext.define('LSP.view.target_by_name.TargetByNameForm', {
-    extend: 'Ext.form.Panel',  
-    alias: 'widget.TargetByNameForm',
-    closable: true,
-    requires: [
+    extend:'Ext.form.Panel',
+    alias:'widget.TargetByNameForm',
+    closable:true,
+    requires:[
         'LSP.view.dropdowns.conceptWikiProteinLookup',
-        'LSP.view.dynamicgrid.DynamicGrid3'
+        'LSP.view.dynamicgrid.DynamicGrid3',
+        'LSP.view.target_by_name.TargetPanel'
     ],
-    layout: {
-          type: 'vbox',
-          align: 'stretch'
-      },
-     initComponent: function() {
-    
+    layout:{
+        type:'vbox',
+        align:'stretch'
+    },
+    initComponent:function () {
+
         this.items = [
-                      {
-                        xtype: 'label',
-                        html: '<font face="verdana" color="grey">Hint: Start typing in protein name and species. E.g. \"Adenosine receptor A2a (Homo sapiens)\"</font>',
-                        labelWidth: 400,
-                        padding: '0 0 0 140' 
-                      },
-                      {
-                        xtype: 'container',
-                        margin: '0 5 5 5',
-                        name: 'form_fields',
-                        layout: {
-                            type: 'column'
-                        },
-                        style: 'background-color: #fff;',
-                        items: [                      
-                              {
-                                xtype: 'conceptWikiProteinLookup'
-                              },
-                              {
-                                xtype: 'button',
-                                padding: '5 5 5 5',
-                                text: 'Search',
-                                itemId: 'TargetByNameSubmit_id',
-                                disabled: true,
-                                action: 'query_target_by_name'
-                              }]
-                        },
-                       {
-                        xtype: 'dynamicgrid3',
-                        itemId: 'TargetByNameGrid_id',
-                        readUrl: '/core_api_calls/protein_info.json',
-                        title: 'Target by name search results',
-                        gridBaseTitle: 'Target by name search results',
-                        flex: 1
-                        }                       
-                ];
+            {
+                xtype:'label',
+                html:'<font face="verdana" color="grey">Hint: Start typing in protein name and species. E.g. \"Adenosine receptor A2a (Homo sapiens)\"</font>',
+                labelWidth:400,
+                padding:'0 0 0 140'
+            },
+            {
+                xtype:'container',
+                margin:'0 5 5 5',
+                name:'form_fields',
+                layout:{
+                    type:'column'
+                },
+                style:'background-color: #fff;',
+                items:[
+                    {
+                        xtype:'conceptWikiProteinLookup'
+                    },
+                    {
+                        xtype:'button',
+                        padding:'5 5 5 5',
+                        text:'Search',
+                        itemId:'TargetByNameSubmit_id',
+                        disabled:true,
+                        action:'query_target_by_name'
+                    }
+                ]
+            },
+            {
+                xtype:'TargetPanel'
+            }
+//                       {
+//                        xtype: 'dynamicgrid3',
+//                        itemId: 'TargetByNameGrid_id',
+//                        readUrl: '/core_api_calls/protein_info.json',
+//                        title: 'Target by name search results',
+//                        gridBaseTitle: 'Target by name search results',
+//                        flex: 1
+//                        }
+        ]
         this.callParent(arguments);
-    }    
+    }
 });
--- public/app/view/target_by_name/TargetPanel.js
+++ public/app/view/target_by_name/TargetPanel.js
@@ -6,23 +6,93 @@
  * To change this template use File | Settings | File Templates.
  */
 Ext.define('LSP.view.target_by_name.TargetPanel', {
-    extend:'Ext.panel.Panel',
+    extend:'Ext.form.Panel',
     alias:'widget.TargetPanel',
     title:'Target Data',
 
-
     initComponent:function () {
-        this.items = [
-            {
-                xtype:'displayfield',
-                fieldLabel:'Target Name'
-            },
-            {
-                xtype:'displayfield',
-                fieldLabel:'Target Type'
+        Ext.apply(this, {
+                items:[
+                    {
+                        xtype:'panel',
+                        itemId:'dataPanel',
+                        hidden:true,
+                        items:[
+
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'Target Name',
+                                name:'target_name'
+                            },
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'Target Type',
+                                name:'target_type'
+                            },
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'Description',
+                                name:'description'
+                            },
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'Organism',
+                                name:'organism'
+                            },
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'Keywords',
+                                name:'keywords'
+                            },
+                            {
+                                xtype:'displayfield',
+                                fieldLabel:'synonyms',
+                                name:'synonyms'
+                            }
+                        ]},
+                    {
+                        xtype:'displayfield',
+                        itemId:'messageField',
+                        hidden:true
+                    }
+                ]
             }
-        ]
+        );
         this.callParent(arguments);
+    },
+
+    startLoading:function () {
+        this.setLoading(true);
+    },
+
+    endLoading:function () {
+        this.setLoading(false);
+    },
+
+    showRecord:function (record) {
+        var mf = this.query('#messageField')[0];
+        mf.hide();
+        var dp = this.query('#dataPanel')[0]
+        dp.show();
+        this.loadRecord(record);
+        this.doLayout();
+    },
+
+    showErrorMessage:function () {
+        var dp = this.query('#dataPanel')[0];
+        dp.hide();
+        var field = this.query('#messageField')[0];
+        field.setRawValue('Server did not respond');
+        field.show();
+    },
+
+    showNoDataMessage:function () {
+        var dp = this.query('#dataPanel')[0];
+        dp.hide();
+        var field = this.query('#messageField')[0];
+        field.setRawValue('No records found within OPS for this search');
+        field.show();
     }
+
 
 });
