5d7f36a1cecd4b8c0163b6b1d986b9aca4f1a39d
Sune Askjaer <sune.askjaer@gmail.com>
2012-05-11 16:16:09 +0200

    Changed syntax of dynamic grid model configuration which did not work in ExtJS 4.1

--- public/app/model/DynamicGrid.js
+++ public/app/model/DynamicGrid.js
@@ -1,26 +1,27 @@
 Ext.define('LSP.model.DynamicGrid', {
     extend:'Ext.data.Model',
-    config:{
+//    config:{
         fields:[],
         proxy:{
             type:'ajax',
             timeout:'180000',
+            suaptest: 'REMOVE',
             api:{
                 read:''  // We configure this in the form controller
             },
             reader:{
                 type:'json',
-                root:'objects',
+                suaptest2: 'REMOVE2',
                 totalProperty:'totalCount'
             }
         }
 
 
-    },
+//    },
 
-    constructor:function (config) {
-        this.initConfig(config);
-        return this;
-    }
+//     constructor:function (config) {
+//         this.initConfig(config);
+//         return this;
+//     }
 
 });
