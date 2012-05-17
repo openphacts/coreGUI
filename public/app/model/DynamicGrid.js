90be2f34a3b723d671a4642cd02c12a4cb1c1887
jeales <jeales@gmail.com>
2012-05-17 17:58:14 +0100

    Fix for problem of api.read url being shared between stores.

--- public/app/model/DynamicGrid.js
+++ public/app/model/DynamicGrid.js
@@ -1,27 +1,4 @@
 Ext.define('LSP.model.DynamicGrid', {
     extend:'Ext.data.Model',
-//    config:{
-        fields:[],
-        proxy:{
-            type:'ajax',
-            timeout:'180000',
-            suaptest: 'REMOVE',
-            api:{
-                read:''  // We configure this in the form controller
-            },
-            reader:{
-                type:'json',
-                suaptest2: 'REMOVE2',
-                totalProperty:'totalCount'
-            }
-        }
-
-
-//    },
-
-//     constructor:function (config) {
-//         this.initConfig(config);
-//         return this;
-//     }
-
+    fields:[]
 });
--- public/app/store/DynamicGrid.js
+++ public/app/store/DynamicGrid.js
@@ -1,5 +1,18 @@
 Ext.define('LSP.store.DynamicGrid', {
     extend:'Ext.data.Store',
     model:'LSP.model.DynamicGrid',
-    fields:[]
+    fields:[],
+    proxy:{
+        type:'ajax',
+        timeout:'180000',
+        suaptest:'REMOVE',
+        api:{
+            read:''  // We configure this in the form controller
+        },
+        reader:{
+            type:'json',
+            suaptest2:'REMOVE2',
+            totalProperty:'totalCount'
+        }
+    }
 });
