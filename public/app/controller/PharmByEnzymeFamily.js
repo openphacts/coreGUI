104ac9b24fb13ef0497d9f5b6a9922ed95c76613
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-08-03 11:58:02 +0100

    rename from selector

--- public/app/controller/PharmByEnzymeFamily.js
+++ public/app/controller/PharmByEnzymeFamily.js
@@ -10,7 +10,7 @@ Ext.define('LSP.controller.PharmByEnzymeFamily', {
             selector:'#pharmByEnzymeFamilyGrid'
         },
         {
-            ref:'formView',
+            ref:'PEForm',
             selector:'PharmEnzymeForm'
         },
         {
