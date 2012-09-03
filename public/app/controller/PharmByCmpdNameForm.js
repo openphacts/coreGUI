44a3f7993fb10e011e4bda1199debd432179eafd
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-09-03 16:29:59 +0100

    check that filter values have been entered

--- public/app/controller/PharmByCmpdNameForm.js
+++ public/app/controller/PharmByCmpdNameForm.js
@@ -1,74 +1,77 @@
+/*
+// Filter model holds the values that the user selected in the filter form view
+// as well as references to the controller and view used when removing the filter
+*/
 Ext.define('LSP.model.Filter', {
-    extend:'Ext.data.Model',
-    fields:['activity', 'condition', 'value', 'unit','filterView','controller'],
+	extend: 'Ext.data.Model',
+	fields: ['activity', 'condition', 'value', 'unit', 'filterView', 'controller'],
 });
 Ext.define('LSP.controller.PharmByCmpdNameForm', {
-        extend:'LSP.controller.grids.DynamicGrid',
-        views:['pharm_by_cmpd_name2.PharmByCmpdNameForm', 'pharm_by_cmpd_name2.PharmByCmpdNameScrollingGrid'],
-        // views:['pharm_by_cmpd_name2.PharmByCmpdNameForm', 'pharm_by_cmpd_name2.PharmByCmpdNameGrid'],
-        refs:[
-            {
-                ref:'gridView', // reference to the view
-                selector:'#pharmByCmpdNameGrid'
-            },
-            {
-                ref:'formView',
-                selector:'PharmByCmpdNameForm'
-            },
-            {
-                ref:'submitButton',
-                selector:'#pharmByCmpdSubmit_id'
-            },
-            {
-                ref:'nextRecordsButton',
-                selector:'PharmByCmpdNameForm dynamicgrid3 #nextRecords'
-            } ,
-            {
-                ref:'lookup',
-                selector:'#pharmByCmpdLookup'
-            },
-			{
-				ref:'filterContainer',
-				selector:'#filterContainer_id'
-			}
-        ],
-	filters:[],
+	extend: 'LSP.controller.grids.DynamicGrid',
+	views: ['pharm_by_cmpd_name2.PharmByCmpdNameForm', 'pharm_by_cmpd_name2.PharmByCmpdNameScrollingGrid'],
+	// views:['pharm_by_cmpd_name2.PharmByCmpdNameForm', 'pharm_by_cmpd_name2.PharmByCmpdNameGrid'],
+	refs: [{
+		ref: 'gridView',
+		// reference to the view
+		selector: '#pharmByCmpdNameGrid'
+	}, {
+		ref: 'formView',
+		selector: 'PharmByCmpdNameForm'
+	}, {
+		ref: 'submitButton',
+		selector: '#pharmByCmpdSubmit_id'
+	}, {
+		ref: 'nextRecordsButton',
+		selector: 'PharmByCmpdNameForm dynamicgrid3 #nextRecords'
+	}, {
+		ref: 'lookup',
+		selector: '#pharmByCmpdLookup'
+	}, {
+		ref: 'filterContainer',
+		selector: '#filterContainer_id'
+	}],
+	filters: [],
 
-        init:function () {
-			console.log('PharmByCmpdNameForm: init()');
-            this.control({
-                'PharmByCmpdNameForm button[action=query_pharm_by_cmpd_name]':{
-                    click:this.submitQuery
-                },
-                'PharmByCmpdNameForm conceptWikiCompoundLookup':{
-                    select:this.enableSubmit
-                },
-                'PharmByCmpdNameForm':{
-                    historyToken:this.handleHistoryToken,
-                    afterrender:this.prepGrid
-                },
-		'PharmByCmpdNameForm button[action=add_filter_form]':{
-		    click:this.addFilterForm
-		},
-		'PharmByCmpdNameForm button[action=add_completed_filter]':{
-		    click:this.addCompletedFilter
-		},
-            });
-        },
+	init: function() {
+		console.log('PharmByCmpdNameForm: init()');
+		this.control({
+			'PharmByCmpdNameForm button[action=query_pharm_by_cmpd_name]': {
+				click: this.submitQuery
+			},
+			'PharmByCmpdNameForm conceptWikiCompoundLookup': {
+				select: this.enableSubmit
+			},
+			'PharmByCmpdNameForm': {
+				historyToken: this.handleHistoryToken,
+				afterrender: this.prepGrid
+			},
+			'PharmByCmpdNameForm button[action=add_filter_form]': {
+				click: this.addFilterForm
+			},
+			'PharmByCmpdNameForm button[action=add_completed_filter]': {
+				click: this.addCompletedFilter
+			},
+		});
+	},
 
-		addCompletedFilter:function(button) {
-			console.log('PharmByCmpdNameForm: addCompletedFilter()');
-		    activity_value = this.getFilterContainer().down('#activity_combobox_id').getValue();
-		    conditions_value = this.getFilterContainer().down('#conditions_combobox_id').getValue();
-		    value_value = this.getFilterContainer().down('#value_textfield_id').getValue();
-		    unit_value = this.getFilterContainer().down('#unit_combobox_id').getValue();
-
-			filter = Ext.create('LSP.model.Filter', {activity: activity_value, condition: conditions_value, value: value_value, unit: unit_value});
-		    this.filters.push(filter);
+	addCompletedFilter: function(button) {
+		console.log('PharmByCmpdNameForm: addCompletedFilter()');
+		activity_value = this.getFilterContainer().down('#activity_combobox_id').getValue();
+		conditions_value = this.getFilterContainer().down('#conditions_combobox_id').getValue();
+		value_value = this.getFilterContainer().down('#value_textfield_id').getValue();
+		unit_value = this.getFilterContainer().down('#unit_combobox_id').getValue();
+		if (activity_value != null && conditions_value != null && value_value != "" && unit_value != null) {
+			filter = Ext.create('LSP.model.Filter', {
+				activity: activity_value,
+				condition: conditions_value,
+				value: value_value,
+				unit: unit_value
+			});
+			this.filters.push(filter);
 			// this is the only way I could find to reference the controller from the model and the view
 			filter.controller = this;
-		
-			filter_view = Ext.create('LSP.view.filter.CompletedFilter',{});
+
+			filter_view = Ext.create('LSP.view.filter.CompletedFilter', {});
 			filter_view.down('#activityLabel_id').setText(activity_value);
 			filter_view.down('#conditionsLabel_id').setText(conditions_value);
 			filter_view.down('#valueLabel_id').setText(value_value);
@@ -79,144 +82,152 @@ Ext.define('LSP.controller.PharmByCmpdNameForm', {
 			this.getFormView().down('#completedFilterContainer_id').add(filter_view);
 			this.getFormView().down('#completedFilterContainer_id').setVisible(true);
 			filter_view.filterModel = filter;
-			filter_view.on({close: this.filterClosed});
-			//TODO assign these values to an object, store them with this controller and display them
-		},
-		
-		/* When a filter is removed from the view also
+			filter_view.on({
+				close: this.filterClosed
+			});
+		} else {
+			Ext.MessageBox.show({
+				title: 'Error',
+				msg: 'Filter options cannot be empty.<br\>Please select a value for each of the filter options.',
+				buttons: Ext.MessageBox.OK,
+				icon: Ext.MessageBox.ERROR
+			});
+		}
+		//TODO assign these values to an object, store them with this controller and display them
+	},
+
+/* When a filter is removed from the view also
 		// remove the model from the controller
 		*/
-		filterClosed:function(filter) {
-			console.log('PharmByCmpdNameForm: filterClosed()');	
-			controller = filter.filterModel.controller;
-			var index = controller.filters.indexOf(filter.filterModel);
-			controller.filters.splice(index, 1);
-		},
+	filterClosed: function(filter) {
+		console.log('PharmByCmpdNameForm: filterClosed()');
+		controller = filter.filterModel.controller;
+		var index = controller.filters.indexOf(filter.filterModel);
+		controller.filters.splice(index, 1);
+	},
 
-		addFilterForm:function(button) {
-			console.log('PharmByCmpdNameForm: addFilterForm()');
-			value = this.getFilterContainer().down('#activity_combobox_id').getValue();
-			// view = Ext.widget('FilterPanel');
-			hide = this.getFilterContainer().hidden;
-			if (hide) {
-				this.getFilterContainer().setVisible(true);	
-			} else {
-				this.getFilterContainer().setVisible(false);
+	addFilterForm: function(button) {
+		console.log('PharmByCmpdNameForm: addFilterForm()');
+		value = this.getFilterContainer().down('#activity_combobox_id').getValue();
+		// view = Ext.widget('FilterPanel');
+		hide = this.getFilterContainer().hidden;
+		if (hide) {
+			this.getFilterContainer().setVisible(true);
+		} else {
+			this.getFilterContainer().setVisible(false);
+		}
+	},
+
+	handleHistoryToken: function(historyTokenObject) {
+		console.log('PharmByCmpdNameForm: handleHistoryToken()');
+		if (historyTokenObject.u) {
+			var dg = this.getGridView();
+			var store = dg.store;
+			if (historyTokenObject.u != store.proxy.extraParams.uri) {
+				store.proxy.extraParams.uri = historyTokenObject.u;
+				store.proxy.reader.uri = historyTokenObject.u;
+				dg.setLoading(true);
+				//loading the store is done after the total results are fetched
+				this.fetchTotalResults();
+				// store.load();
 			}
-		},
-
-        handleHistoryToken:function (historyTokenObject) {
-            console.log('PharmByCmpdNameForm: handleHistoryToken()');
-            if (historyTokenObject.u) {
-                var dg = this.getGridView();
-                var store = dg.store;
-                if (historyTokenObject.u != store.proxy.extraParams.uri) {
-                    store.proxy.extraParams.uri = historyTokenObject.u;
-					store.proxy.reader.uri = historyTokenObject.u;
-                    dg.setLoading(true);
-					//loading the store is done after the total results are fetched
-					this.fetchTotalResults();
-                    // store.load();
-                }
-            } else if (historyTokenObject.s) {
-                var lookup = this.getLookup();
-                lookup.setRawValue(historyTokenObject.s);
-                lookup.doQuery(historyTokenObject.s);
-            }
-        },
-		fetchTotalResults:function() {
-			console.log('PharmByCmpdNameForm: fetchTotalResults()');
-			var grid_view = this.getGridView();
-			var grid_store = grid_view.getStore();
-			var form = this.getFormView();
-	        var button = this.getSubmitButton();
-			countStore = Ext.create('LDA.store.CompoundPharmacologyCountStore');
-			countStore.uri = grid_store.proxy.reader.uri;
-				countStore.load(function(records, operation, success) {
-					total = operation.response.result.primaryTopic.compoundPharmacologyTotalResults;
-					grid_store.proxy.reader.total_count = total;
-					// we have the total number of results now and the proxy reader knows what it is so
-					// fetch the first page of results
-					if (total == 0) {
-							            grid_view.setTitle(grid_view.gridBaseTitle + ' - No records found within OPS for this search!');
-						grid_view.down('#sdfDownload_id').disable();
-						grid_view.down('#sdfDownloadProxy_id').setText('Prepare SD-file download');
-				        grid_view.down('#sdfDownloadProxy_id').disable();
-				        button.enable();
-				        grid_view.setLoading(false);
-							            Ext.MessageBox.show({
-							                title:'Info',
-							                msg:'The OPS system does not contain any data that match this search.',
-							                buttons:Ext.MessageBox.OK,
-							                icon:Ext.MessageBox.INFO
-							            });
-							        } else {
-						// for pagianted grid use this
-						// grid_store.load();
-						grid_store.guaranteeRange(0,49);
-					}		
+		} else if (historyTokenObject.s) {
+			var lookup = this.getLookup();
+			lookup.setRawValue(historyTokenObject.s);
+			lookup.doQuery(historyTokenObject.s);
+		}
+	},
+	fetchTotalResults: function() {
+		console.log('PharmByCmpdNameForm: fetchTotalResults()');
+		var grid_view = this.getGridView();
+		var grid_store = grid_view.getStore();
+		var form = this.getFormView();
+		var button = this.getSubmitButton();
+		countStore = Ext.create('LDA.store.CompoundPharmacologyCountStore');
+		countStore.uri = grid_store.proxy.reader.uri;
+		countStore.load(function(records, operation, success) {
+			total = operation.response.result.primaryTopic.compoundPharmacologyTotalResults;
+			grid_store.proxy.reader.total_count = total;
+			// we have the total number of results now and the proxy reader knows what it is so
+			// fetch the first page of results
+			if (total == 0) {
+				grid_view.setTitle(grid_view.gridBaseTitle + ' - No records found within OPS for this search!');
+				grid_view.down('#sdfDownload_id').disable();
+				grid_view.down('#sdfDownloadProxy_id').setText('Prepare SD-file download');
+				grid_view.down('#sdfDownloadProxy_id').disable();
+				button.enable();
+				grid_view.setLoading(false);
+				Ext.MessageBox.show({
+					title: 'Info',
+					msg: 'The OPS system does not contain any data that match this search.',
+					buttons: Ext.MessageBox.OK,
+					icon: Ext.MessageBox.INFO
 				});
-		},
+			} else {
+				// for pagianted grid use this
+				// grid_store.load();
+				grid_store.guaranteeRange(0, 49);
+			}
+		});
+	},
 
-        prepGrid:function () {
-            console.log('PharmByCmpdNameForm: prepGrid()');
-            var grid_view = this.getGridView();
-	        var store = grid_view.getStore();
-	        store.on('prefetch', this.storeLoadComplete, this);
-	        // store.setPage(1);
-            // var add_next_button = this.getNextRecordsButton();
-            // add_next_button.on('click', function () {
-            //     var form_values = add_next_button.up('form').getValues();
-            //     grid_controller.addNextRecords(grid_view, form_values);
-            // });
-            // 
-            // grid_view.store.proxy.actionMethods = {read:'POST'};
-            // grid_view.store.proxy.api.read = grid_view.readUrl;
-//            grid_view.store.proxy.params = {offset:0, limit:100};
+	prepGrid: function() {
+		console.log('PharmByCmpdNameForm: prepGrid()');
+		var grid_view = this.getGridView();
+		var store = grid_view.getStore();
+		store.on('prefetch', this.storeLoadComplete, this);
+		// store.setPage(1);
+		// var add_next_button = this.getNextRecordsButton();
+		// add_next_button.on('click', function () {
+		//     var form_values = add_next_button.up('form').getValues();
+		//     grid_controller.addNextRecords(grid_view, form_values);
+		// });
+		// 
+		// grid_view.store.proxy.actionMethods = {read:'POST'};
+		// grid_view.store.proxy.api.read = grid_view.readUrl;
+		//            grid_view.store.proxy.params = {offset:0, limit:100};
+		// grid_view.store.on('load', this.storeLoadComplete, this);
+	},
 
-            // grid_view.store.on('load', this.storeLoadComplete, this);
-        },
+	storeLoadComplete: function(store, records, success) {
+		console.log('PharmByCmpdNameForm: storeLoadComplete()');
+		grid_view = this.getGridView();
+		grid_view.down('#sdfDownload_id').disable();
+		grid_view.down('#sdfDownloadProxy_id').setText('Prepare SD-file download');
+		grid_view.down('#sdfDownloadProxy_id').enable();
+		// var controller = this.getController('LSP.controller.grids.DynamicGrid');
+		// var grid_view = this.getGridView();
+		var form = this.getFormView();
+		var button = this.getSubmitButton();
 
-        storeLoadComplete:function (store, records, success) {
-            console.log('PharmByCmpdNameForm: storeLoadComplete()');
-			grid_view = this.getGridView();
-			grid_view.down('#sdfDownload_id').disable();
-			grid_view.down('#sdfDownloadProxy_id').setText('Prepare SD-file download');
-	        grid_view.down('#sdfDownloadProxy_id').enable();
-            // var controller = this.getController('LSP.controller.grids.DynamicGrid');
-            // var grid_view = this.getGridView();
-            var form = this.getFormView();
-            var button = this.getSubmitButton();
+		// controller.storeLoad(grid_view, success);
+		// form.doLayout();
+		button.enable();
+		// grid_view.doLayout();
+		// grid_view.doComponentLayout();
+		grid_view.setLoading(false);
+		this.callParent();
+	},
 
-            // controller.storeLoad(grid_view, success);
-            // form.doLayout();
-            button.enable();
-            // grid_view.doLayout();
-            // grid_view.doComponentLayout();
-            grid_view.setLoading(false);
-			this.callParent();
-        },
+	createGridColumns: function() {
+		console.log('PharmByCmpdNameForm: createGridColumns()');
+		var grid_controller = this.getController('LSP.controller.grids.DynamicGrid');
+		var this_gridview = this.getGridView();
+		grid_controller.storeLoad(this_gridview);
+	},
 
-        createGridColumns:function () {
-	        console.log('PharmByCmpdNameForm: createGridColumns()');
-            var grid_controller = this.getController('LSP.controller.grids.DynamicGrid');
-            var this_gridview = this.getGridView();
-            grid_controller.storeLoad(this_gridview);
-        },
+	enableSubmit: function(compoundLookup) {
+		console.log('PharmByCmpdNameForm: enableSubmit()');
+		var form = this.getFormView();
+		var button = form.query('button[action=query_pharm_by_cmpd_name]')[0];
+		button.enable();
+	},
 
-        enableSubmit:function (compoundLookup) {
-			console.log('PharmByCmpdNameForm: enableSubmit()');
-            var form = this.getFormView();
-            var button = form.query('button[action=query_pharm_by_cmpd_name]')[0];
-            button.enable();
-        },
-
-        submitQuery:function (button) {
-			console.log('PharmByCmpdNameForm: submitQuery()');
-            var form = button.up('form');
-            button.disable();
-            var values = form.getValues();
-            Ext.History.add('!p=PharmByCmpdNameForm&u=' + values.compound_uri);
-        }
-    }
-);
+	submitQuery: function(button) {
+		console.log('PharmByCmpdNameForm: submitQuery()');
+		var form = button.up('form');
+		button.disable();
+		var values = form.getValues();
+		Ext.History.add('!p=PharmByCmpdNameForm&u=' + values.compound_uri);
+	}
+});
