26bcf423c9f5c7c9178908c20ab40759d81e3585
Rishi Ramgolam <ramgolam@aqnowledge.com>
2012-09-24 12:50:05 +0100

    Pharm by Target provenance renderer

--- public/app/view/pharm_by_target_name2/PharmByTargetNameScrollingGrid.js
+++ public/app/view/pharm_by_target_name2/PharmByTargetNameScrollingGrid.js
@@ -64,54 +64,130 @@ Ext.define('LSP.view.pharm_by_target_name2.PharmByTargetNameScrollingGrid', {
             },
             {
                 header:'Smiles',
-                dataIndex:'compound_smiles'
+                dataIndex:'compound_smiles',
+                renderer: provenanceRenderer,
+                align: 'center'
+
             },
             {
                 header:'Std Value',
-                dataIndex:'activity_standard_value'
+                dataIndex:'activity_standard_value',
+                renderer: provenanceRenderer,
+                align: 'center'
+
             },
             {
                 header:'Chemspider ID',
                 dataIndex:'cs_compound_uri',
-				sortable:false
+				sortable:false,
+                renderer:provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Inchi key',
-                dataIndex:'compound_inchikey'
+                dataIndex:'compound_inchikey',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Std Type',
-                dataIndex:'activity_activity_type'
+                dataIndex:'activity_activity_type',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Std Unit',
-                dataIndex:'activity_standard_units'
+                dataIndex:'activity_standard_units',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Target Name',
-                dataIndex:'target_pref_label'
+                dataIndex:'target_pref_label',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Relation',
-                dataIndex:'activity_relation'
+                dataIndex:'activity_relation',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Molweight',
-                dataIndex:'compound_full_mwt'
+                dataIndex:'compound_full_mwt',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Inchi',
-                dataIndex:'compound_inchi'
+                dataIndex:'compound_inchi',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
             {
                 header:'Compound name',
-                dataIndex:'compound_pref_label'
+                dataIndex:'compound_pref_label',
+                renderer: provenanceRenderer,
+                align: 'center'
             },
 			{
 				header: 'Compound cw',
-				dataIndex: 'cw_compound_uri'
+				dataIndex: 'cw_compound_uri',
+                renderer: provenanceRenderer,
+                align: 'center'
 			}
-            ]
+            ],
+
+            toggleProv:function (val) {
+                prov = val;
+                console.log(" Show provenance : " + prov );
+                this.doLayout();
+            }
         
     }
 );
+
+var prov = false;
+
+function provenanceRenderer (data, cell, record, rowIndex, columnIndex, store) {
+
+    //if (LDAProvenanceMode != LDA.helper.LDAConstants.LDA_PROVENANCE_OFF) {
+    if (prov) {
+
+        var recdata = this.columns[columnIndex].dataIndex;
+        recdata += '_src';
+        var source = record.data[recdata];
+        var cls = LDA.helper.LDAConstants.LDA_SRC_CLS_MAPPINGS[source];
+        if (!cls) {
+            cls = 'defaultValue';
+        }
+        var iconCls = cls + 'Icon';
+        //console.log(iconCls);
+        cls += LDAProvenanceMode;
+        if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_COLOUR) {
+
+            if (record.data[recdata] && data){
+
+                // return '<div class="' + cls + '">' + data + '</div>' + '<br>' + record.data[recdata];
+                return '<div class="' + cls + '">' + data + '</div>' + '<br>' + '<a href="' + record.data[recdata] +'">' +'<img class="' + iconCls + '" height="15" width="15"/>' + '</a>'                    ;
+
+            } else {
+
+                return '<div class="' + cls + '">' + data + '</div>'
+
+            }
+
+        }
+        //else if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_ICON) {
+        //this needs an img adding in
+        //    return '<div class="' + cls + '">' + data + '</div>';
+        //} else if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_TEXT) {
+        //    return '<div class="' + cls + '">' + data + ' (' + source + ')</div>';
+        //}
+    } else {
+        return data;
+    }
+    return data;
+};
+
