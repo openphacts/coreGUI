af696c6c0fe2a39e1349718e93374f01b08dc9f4
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-10-12 10:00:57 +0100

    chemspider only for compound searches

--- public/app/view/cmpd_by_name/CmpdByNameForm.js
+++ public/app/view/cmpd_by_name/CmpdByNameForm.js
@@ -1,60 +1,61 @@
 Ext.define('LSP.view.cmpd_by_name.CmpdByNameForm', {
-    extend:'Ext.form.Panel',
-    alias:'widget.CmpdByNameForm',
-    closable:true,
+    extend: 'Ext.form.Panel',
+    alias: 'widget.CmpdByNameForm',
+    closable: true,
     header: false,
-    requires:[
-        'LSP.view.dropdowns.conceptWikiCompoundLookup',
-        'LSP.view.dynamicgrid.DynamicGrid',
-        'LSP.view.cmpd_by_name.CmpdByNameSingleDisplayForm'
-    ],
-    layout:{
-        type:'vbox',
-        align:'stretch'
+    requires: ['LSP.view.dropdowns.conceptWikiCompoundLookup', 'LSP.view.dynamicgrid.DynamicGrid', 'LSP.view.cmpd_by_name.CmpdByNameSingleDisplayForm'],
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
     },
 
-    initComponent:function () {
+    initComponent: function() {
         var me = this;
 
         Ext.applyIf(me, {
-            items:[
-                {
-                    xtype:'label',
-                    html:'<span style="font-family: verdana; color: grey; ">Hint: Type in compound name. E.g. \"Aspirin\"</span>',
-                    labelWidth:400,
-                    padding:'5 0 0 140'
+            items: [{
+                xtype: 'label',
+                html: '<span style="font-family: verdana; color: grey; ">Hint: Type in compound name. E.g. \"Aspirin\"</span>',
+                labelWidth: 400,
+                padding: '5 0 0 140'
+            }, {
+                xtype: 'container',
+                margin: '0 5 5 5',
+                name: 'form_fields',
+                layout: {
+                    type: 'column'
                 },
-                {
-                    xtype:'container',
-                    margin:'0 5 5 5',
-                    name:'form_fields',
-                    layout:{
-                        type:'column'
-                    },
-                    style:'background-color: #fff;',
-                    items:[
-                          Ext.create('CW.view.ConceptWikiLookup', {
-	                        xtype:'conceptWikiLookup',
-	                        fieldLabel:'Compound name',
-	                        itemId: 'compoundByNameLookup',
-				store: Ext.create('CW.store.ConceptWikiLookup', {}),
-	                        name: 'compound_uri',
-	                        cwTagUuid: '07a84994-e464-4bbf-812a-a4b96fa3d197'   // This is the ConceptWiki tag uuid for proteins. Must be set to use method!
-	                    }),
-                        {
-                            xtype:'button',
-                            padding:'5 5 5 5',
-                            text:'Search...',
-                            itemId:'CmpdByNameSubmit_id',
-                            disabled:true,
-                            action:'query_cmpd_by_name'
+                style: 'background-color: #fff;',
+                items: [
+                Ext.create('CW.view.ConceptWikiLookup', {
+                    xtype: 'conceptWikiLookup',
+                    fieldLabel: 'Compound name',
+                    itemId: 'compoundByNameLookup',
+                    store: Ext.create('CW.store.ConceptWikiLookup', {
+                        proxy: {
+                            type: 'jsonp',
+                            timeout: 5000,
+                            url: CW.config.Settings.searchByTagUrl,
+                            reader: Ext.create('CW.helper.ConceptWikiJSONReader'),
+                            extraParams: {
+                                'branch': 4 // Only show species results from swissprot
+                            }
                         }
-                    ]},
-                {
-                    xtype:'CmpdByNameSingleDisplayForm',
-                    flex:1
-                }
-            ]
+                    }),
+                    name: 'compound_uri',
+                    cwTagUuid: '07a84994-e464-4bbf-812a-a4b96fa3d197' // This is the ConceptWiki tag uuid for proteins. Must be set to use method!
+                }), {
+                    xtype: 'button',
+                    padding: '5 5 5 5',
+                    text: 'Search...',
+                    itemId: 'CmpdByNameSubmit_id',
+                    disabled: true,
+                    action: 'query_cmpd_by_name'
+                }]
+            }, {
+                xtype: 'CmpdByNameSingleDisplayForm',
+                flex: 1
+            }]
         });
         this.callParent(arguments);
     }
--- public/app/view/pharm_by_cmpd_name2/PharmByCmpdNameForm.js
+++ public/app/view/pharm_by_cmpd_name2/PharmByCmpdNameForm.js
@@ -1,116 +1,128 @@
 Ext.define('LSP.view.pharm_by_cmpd_name2.PharmByCmpdNameForm', {
-	extend: 'Ext.form.Panel',
-	alias: 'widget.PharmByCmpdNameForm',
-	closable: true,
-	requires: ['LSP.view.dropdowns.conceptWikiCompoundLookup', 'LDA.helper.DynamicPagingToolbar', 'LSP.view.filter.FilterForm', 'LDA.helper.LDAConstants'],
+    extend: 'Ext.form.Panel',
+    alias: 'widget.PharmByCmpdNameForm',
+    closable: true,
+    requires: ['LSP.view.dropdowns.conceptWikiCompoundLookup', 'LDA.helper.DynamicPagingToolbar', 'LSP.view.filter.FilterForm', 'LDA.helper.LDAConstants'],
     header: false,
     layout: {
-		type: 'vbox',
-		align: 'stretch'
-	},
-	// Use a PagingGridScroller (this is interchangeable with a PagingToolbar)
-	// verticalScrollerType: 'dynamicpagingtoolbar',
-	// do not reset the scrollbar when the view refreshs
-	invalidateScrollerOnRefresh: false,
-	// infinite scrolling does not support selection
-	disableSelection: true,
-	initComponent: function() {
-		console.log('PharmByCmpdNameForm: initComponent()');
-		this.items = [{
-			xtype: 'label',
-			html: '<span style="font-family: verdana; color: grey; ">Hint: Type in compound name. E.g. \"Aspirin\"</span>',
-			labelWidth: 400,
-			padding: '5 0 0 140'
-		}, {
-			xtype: 'container',
-			margin: '0 5 5 5',
-			name: 'form_fields',
-			layout: {
-				type: 'column'
-			},
-			style: 'background-color: #fff;',
-			items: [{
-				name: 'cmpd_uuid',
-				xtype: 'hidden',
-				value: ''
-			}, Ext.create('CW.view.ConceptWikiLookup', {
-	                        xtype:'conceptWikiLookup',
-	                        fieldLabel:'Compound name',
-	                        itemId: 'compoundByNameLookup',
-				store: Ext.create('CW.store.ConceptWikiLookup', {}),
-	                        name: 'compound_uri',
-	                        cwTagUuid: '07a84994-e464-4bbf-812a-a4b96fa3d197'   // This is the ConceptWiki tag uuid for proteins. Must be set to use method!
-	                }),
-{
-				xtype: 'button',
-				itemId: 'pharmByCmpdSubmit_id',
-				padding: '5 5 5 5',
-				text: 'Search...',
-				disabled: true,
-				action: 'query_pharm_by_cmpd_name'
-			}]
-		}, {
-			xtype: 'container',
-			margin: '0 5 5 5',
-			name: 'filter_fields',
-			layout: {
-				type: 'column'
-			},
-			style: 'background-color: #fff;',
-			items: [{
-				xtype: 'button',
-				itemId: 'addFilterButton_id',
-				iconCls: 'icon-new',
-				padding: '5 5 5 5',
-				tooltip: 'Show or hide filter selector',
-				action: 'add_filter_form'
-			}, {
-				xtype: 'label',
-				forId: 'addFilterButton_id',
-				text: 'Filter',
-				margin: '5 5 5 5'
-			},{
+        type: 'vbox',
+        align: 'stretch'
+    },
+    // Use a PagingGridScroller (this is interchangeable with a PagingToolbar)
+    // verticalScrollerType: 'dynamicpagingtoolbar',
+    // do not reset the scrollbar when the view refreshs
+    invalidateScrollerOnRefresh: false,
+    // infinite scrolling does not support selection
+    disableSelection: true,
+    initComponent: function() {
+        console.log('PharmByCmpdNameForm: initComponent()');
+        this.items = [{
+            xtype: 'label',
+            html: '<span style="font-family: verdana; color: grey; ">Hint: Type in compound name. E.g. \"Aspirin\"</span>',
+            labelWidth: 400,
+            padding: '5 0 0 140'
+        }, {
+            xtype: 'container',
+            margin: '0 5 5 5',
+            name: 'form_fields',
+            layout: {
+                type: 'column'
+            },
+            style: 'background-color: #fff;',
+            items: [{
+                name: 'cmpd_uuid',
+                xtype: 'hidden',
+                value: ''
+            },
+            Ext.create('CW.view.ConceptWikiLookup', {
+                xtype: 'conceptWikiLookup',
+                fieldLabel: 'Compound name',
+                itemId: 'compoundByNameLookup',
+                store: Ext.create('CW.store.ConceptWikiLookup', {
+                    proxy: {
+                        type: 'jsonp',
+                        timeout: 5000,
+                        url: CW.config.Settings.searchByTagUrl,
+                        reader: Ext.create('CW.helper.ConceptWikiJSONReader'),
+                        extraParams: {
+                            'branch': 4 // Only show species results from swissprot
+                        }
+                    }
+                }),
+                name: 'compound_uri',
+                cwTagUuid: '07a84994-e464-4bbf-812a-a4b96fa3d197' // This is the ConceptWiki tag uuid for proteins. Must be set to use method!
+            }), {
+                xtype: 'button',
+                itemId: 'pharmByCmpdSubmit_id',
+                padding: '5 5 5 5',
+                text: 'Search...',
+                disabled: true,
+                action: 'query_pharm_by_cmpd_name'
+            }]
+        }, {
+            xtype: 'container',
+            margin: '0 5 5 5',
+            name: 'filter_fields',
+            layout: {
+                type: 'column'
+            },
+            style: 'background-color: #fff;',
+            items: [{
+                xtype: 'button',
+                itemId: 'addFilterButton_id',
+                iconCls: 'icon-new',
+                padding: '5 5 5 5',
+                tooltip: 'Show or hide filter selector',
+                action: 'add_filter_form'
+            }, {
+                xtype: 'label',
+                forId: 'addFilterButton_id',
+                text: 'Filter',
+                margin: '5 5 5 5'
+            }, {
                 xtype: 'radiogroup',
                 width: 200,
                 fieldLabel: 'Provenance',
                 itemId: 'provId',
                 margin: '5 5 5 65',
 
-                items: [
-                    { boxLabel: 'On', name: 'prov', inputValue: true },
-                    { boxLabel: 'Off', name: 'prov', inputValue: false, checked: true}]
-            },{
-               xtype: 'button',
-               name: 'provHelp',
-               margin: '5 0 0 0',
-               iconCls:'provenanceHelpIcon',
-               tooltip: 'Provenance Datasources <br><br><p class="conceptWikiValueColour"> - ConceptWiki </p> ' +
-                   '<br><p class="chemspiderValueColour"> - ChemSpider </p>' +
-                   '<br><p class="drugbankValueColour"> - Drugbank </p>' +
-                   '<br><p class="chemblValueColour"> - Chembl</p>'
-            }
-            ]
-		},{
-			xtype: 'FilterPanel',
-			itemId: 'filterContainer_id',
-			margin: '0 5 5 5',
-			name: 'filter_fields',
-			hidden: true
-		}, 
-		 {
-			xtype: 'container',
-			itemId: 'completedFilterContainer_id',
-			margin: '0 5 5 5',
-			name: 'completed_filter_container',
-			hidden: true
-		},{
-			xtype: 'PharmByCmpdNameScrollingGrid',
-			itemId: 'pharmByCmpdNameGrid',
-			title: 'Pharmacology by Compound name search results',
-			gridBaseTitle: 'Pharmacology by Compound name search results',
-			flex: 1
-		}];
-		this.callParent(arguments);
-	}
+                items: [{
+                    boxLabel: 'On',
+                    name: 'prov',
+                    inputValue: true
+                }, {
+                    boxLabel: 'Off',
+                    name: 'prov',
+                    inputValue: false,
+                    checked: true
+                }]
+            }, {
+                xtype: 'button',
+                name: 'provHelp',
+                margin: '5 0 0 0',
+                iconCls: 'provenanceHelpIcon',
+                tooltip: 'Provenance Datasources <br><br><p class="conceptWikiValueColour"> - ConceptWiki </p> ' + '<br><p class="chemspiderValueColour"> - ChemSpider </p>' + '<br><p class="drugbankValueColour"> - Drugbank </p>' + '<br><p class="chemblValueColour"> - Chembl</p>'
+            }]
+        }, {
+            xtype: 'FilterPanel',
+            itemId: 'filterContainer_id',
+            margin: '0 5 5 5',
+            name: 'filter_fields',
+            hidden: true
+        }, {
+            xtype: 'container',
+            itemId: 'completedFilterContainer_id',
+            margin: '0 5 5 5',
+            name: 'completed_filter_container',
+            hidden: true
+        }, {
+            xtype: 'PharmByCmpdNameScrollingGrid',
+            itemId: 'pharmByCmpdNameGrid',
+            title: 'Pharmacology by Compound name search results',
+            gridBaseTitle: 'Pharmacology by Compound name search results',
+            flex: 1
+        }];
+        this.callParent(arguments);
+    }
 
 });
