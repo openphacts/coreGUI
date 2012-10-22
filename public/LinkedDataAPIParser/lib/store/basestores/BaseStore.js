5f411e2f14e1a7f3077267cd1dae961f1e70f5f9
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-10-22 16:27:45 +0100

    grid sorting for resutls less than page size

--- public/LinkedDataAPIParser/lib/store/basestores/BaseStore.js
+++ public/LinkedDataAPIParser/lib/store/basestores/BaseStore.js
@@ -5,6 +5,7 @@ Ext.define('LDA.store.basestores.BaseStore', {
 	uri: '',
 	BASE_URL: '',
 	remoteSort: true,
+	//gridController: undefined,
 	stringEncoder: Ext.create('LDA.helper.JamesQueryStringEncoder'),
 	proxy: {
 		type: 'jsonp',
@@ -12,10 +13,12 @@ Ext.define('LDA.store.basestores.BaseStore', {
 		startParam: undefined,
 		limitParam: undefined,
 		pageParam: undefined,
+		//gridStore: this,
 		//this is the only query param handled natively by the proxy, all others are handled in store config below.
 		callbackKey: '_callback',
 		listeners: { // configure listener
 			exception: function(request, operation, options) {
+				//this.gridStore.gridController.getGridView.setLoading(false);
 				// this block is reached on any exception
 				Ext.Msg.show({
 					title: '',
@@ -26,6 +29,29 @@ Ext.define('LDA.store.basestores.BaseStore', {
 			}
 		}
 	},
+	// Allow sorting with less than pageSize of results
+	// Supposedly fixed according to http://www.sencha.com/forum/showthread.php?190791-4.1-RC1-Remote-sort-from-a-buffered-store-fails-on-small-data-sets
+	// but the behaviour still persists. TODO check EXTJS updates to see if
+	// it gets fixed
+	prefetchPage: function(page, options) {
+        var me = this,
+            pageSize = me.pageSize || me.defaultPageSize,
+            start = (page - 1) * me.pageSize,
+            total = me.totalCount;
+
+        // No more data to prefetch.
+	// changed this line by adding check for count greater than page size
+        if (total !== undefined && me.getCount() === total && me.getCount() > pageSize) {
+            return;
+        }
+
+        // Copy options into a new object so as not to mutate passed in objects
+        me.prefetch(Ext.applyIf({
+            page     : page,
+            start    : start,
+            limit    : pageSize
+        }, options));
+    },
 
 	listeners: {
 		//this is used to construct the proxy url before the load is done
