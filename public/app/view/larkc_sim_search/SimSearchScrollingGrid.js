0f28abaf6f7873c2029dc0c5dcc08802b412db2f
Rishi Ramgolam <ramgolam@aqnowledge.com>
2012-11-01 14:47:12 +0000

    Provenance renderer added to Structure search grid

--- public/app/view/larkc_sim_search/SimSearchScrollingGrid.js
+++ public/app/view/larkc_sim_search/SimSearchScrollingGrid.js
@@ -50,70 +50,135 @@ Ext.define('LSP.view.larkc_sim_search.SimSearchScrollingGrid', {
     }, {
         header: 'Compound name',
         dataIndex: 'compound_pref_label',
+        renderer:provenanceRenderer,
         width: 180,
         tdCls: 'wrap gridDescriptiveRowPadding'
     }, {
         header: 'Molecular Formula',
         dataIndex: 'molform',
+        renderer:provenanceRenderer,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'SMILES',
         dataIndex: 'compound_smiles',
+        renderer:provenanceRenderer,
         width: 135,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'InChi',
         dataIndex: 'inchi',
+        renderer:provenanceRenderer,
         width: 135,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'InChi Key',
         dataIndex: 'inchi_key',
+        renderer:provenanceRenderer,
         width: 135,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'ALogP',
         dataIndex: 'alogp',
+        renderer:provenanceRenderer,
         width: 60,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: '# HBA',
         dataIndex: 'hba',
+        renderer:provenanceRenderer,
         width: 60,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: '# HBD',
         dataIndex: 'hbd',
+        renderer:provenanceRenderer,
         width: 60,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'Mol Weight',
         dataIndex: 'full_mwt',
+        renderer:provenanceRenderer,
         width: 70,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: '# RTB',
         dataIndex: 'rtb',
+        renderer:provenanceRenderer,
         width: 60,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'MW Freebase',
         dataIndex: 'mw_freebase',
+        renderer:provenanceRenderer,
         align: 'center',
         tdCls: 'gridRowPadding'
     }, {
         header: 'Melting Point',
         dataIndex: 'meltingPoint',
+        renderer:provenanceRenderer,
         width: 140,
         tdCls: 'wrap gridDescriptiveRowPadding'
     }]
+    ,
+    toggleProv:function (val) {
+        simSearchProv = val;
+        console.log(" Show provenance : " + simSearchProv);
+        this.doLayout();
+    }
 });
+
+
+var simSearchProv = false;
+
+function provenanceRenderer(data, cell, record, rowIndex, columnIndex, store) {
+
+    //if (LDAProvenanceMode != LDA.helper.LDAConstants.LDA_PROVENANCE_OFF) {
+    if (simSearchProv) {
+
+        var recdata = this.columns[columnIndex].dataIndex;
+        var itemdata = recdata + '_item';
+        recdata += '_src';
+        var source = record.data[recdata];
+        var cls = LDA.helper.LDAConstants.LDA_SRC_CLS_MAPPINGS[source];
+        if (!cls) {
+            cls = 'defaultValue';
+        }
+        var iconCls = cls + 'Icon';
+        iconCls = '/assets/' + iconCls + '.png';
+        //console.log(iconCls);
+        cls += LDAProvenanceMode;
+        if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_COLOUR) {
+
+            if (record.data[recdata] && data) {
+
+                // return '<div class="' + cls + '">' + data + '</div>' + '<br>' + record.data[recdata];
+                return '<div class="' + cls + '">' + data + '</div>' + '<br>' + '<a href="' + record.data[itemdata] + '" target="_blank">' + '<img src="' + iconCls + '" height="15" width="15"/>' + '</a>';
+
+            } else {
+
+                return '<div class="' + cls + '">' + data + '</div>'
+
+            }
+
+        }
+        //else if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_ICON) {
+        //this needs an img adding in
+        //    return '<div class="' + cls + '">' + data + '</div>';
+        //} else if (LDAProvenanceMode == LDA.helper.LDAConstants.LDA_PROVENANCE_TEXT) {
+        //    return '<div class="' + cls + '">' + data + ' (' + source + ')</div>';
+        //}
+    } else {
+        return data;
+    }
+    return data;
+}
+;
