cb8ad169304caf9b6ae1b9565d46d2741a30c154
Rishi Ramgolam <ramgolam@aqnowledge.com>
2012-11-09 16:31:59 +0000

    Pharm by cmp provenance toggle var moved inside class

--- public/app/view/pharm_by_cmpd_name2/PharmByCmpdNameScrollingGrid.js
+++ public/app/view/pharm_by_cmpd_name2/PharmByCmpdNameScrollingGrid.js
@@ -146,21 +146,22 @@ Ext.define('LSP.view.pharm_by_cmpd_name2.PharmByCmpdNameScrollingGrid', {
 
             }
         ],
+
+        compound_prov: false,
+
         toggleProv:function (val) {
-            compound_prov = val;
-            console.log(" Show provenance : " + compound_prov);
+            this.compound_prov = val;
+            console.log(" Show provenance : " + this.compound_prov);
             this.doLayout();
         }
     }
 );
 
-var compound_prov = false;
-
 function compoundProvenanceRenderer(data, cell, record, rowIndex, columnIndex, store) {
 	//console.log("Compound Pharmacology provenance renderer");
 
     //if (LDAProvenanceMode != LDA.helper.LDAConstants.LDA_PROVENANCE_OFF) {
-    if (compound_prov) {
+    if (this.compound_prov) {
 
         var recdata = this.columns[columnIndex].dataIndex;
         var itemdata = recdata + '_item';
