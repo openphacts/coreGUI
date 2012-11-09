51d5c0f48743a67bcec280f49c1b99f52fc21cdb
Rishi Ramgolam <ramgolam@aqnowledge.com>
2012-11-09 16:34:55 +0000

    Pharm by enz provenance toggle var moved inside class

--- public/app/view/pharm_by_enzyme_family/PharmByEnzymeFamilyScrollingGrid.js
+++ public/app/view/pharm_by_enzyme_family/PharmByEnzymeFamilyScrollingGrid.js
@@ -137,21 +137,22 @@ Ext.define('LSP.view.pharm_by_enzyme_family.PharmByEnzymeFamilyScrollingGrid', {
             ]
 
         },
+
+        enzyme_prov: false,
+
         toggleProv:function (val) {
-            enzyme_prov = val;
-            console.log(" Show provenance : " + enzyme_prov );
+            this.enzyme_prov = val;
+            console.log(" Show provenance : " + this.enzyme_prov );
             this.doLayout();
         }
     }
 );
 
-var enzyme_prov = false;
-
 function enzymeProvenanceRenderer (data, cell, record, rowIndex, columnIndex, store) {
 	//console.log("Enzyme Pharmacology provenance renderer");
 
     //if (LDAProvenanceMode != LDA.helper.LDAConstants.LDA_PROVENANCE_OFF) {
-    if (enzyme_prov) {
+    if (this.enzyme_prov) {
 
         var recdata = this.columns[columnIndex].dataIndex;
         var itemdata = recdata + '_item';
