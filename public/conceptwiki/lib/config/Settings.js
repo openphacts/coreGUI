cdf2c7e82eccff16690664a6d878b8ac86c61b62
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-12-13 10:55:57 +0000

    move the concept wiki url into a constant on the rails side

--- public/conceptwiki/lib/config/Settings.js
+++ public/conceptwiki/lib/config/Settings.js
@@ -1,7 +1,7 @@
 Ext.define('CW.config.Settings', {
     singleton: true,
-    searchByTagUrl: 'http://ops.conceptwiki.org/web-ws/concept/search/byTag',
-    getConceptUrl: 'http://ops.conceptwiki.org/web-ws/concept/get',
+    searchByTagUrl: cwBaseUrl + '/web-ws/concept/search/byTag',
+    getConceptUrl: cwBaseUrl + '/web-ws/concept/get',
     base_ops_uri: 'http://www.conceptwiki.org/concept/',
     lang_code: "en"
 });
