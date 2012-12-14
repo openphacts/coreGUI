553dd6ef66d9bcdd96250c869622a8490a58615d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-12-14 09:33:38 +0000

    move database.yml to example file and add to gitignore. change feedback mail recipients

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,14 +1,11 @@
 class FeedbackMailer < ActionMailer::Base
-  default :from => AppSettings.config["mail"]["user"]
-  default :bcc => AppSettings.config["mail"]["to"]
-  #, "bugs@openphacts.org"
-  default :subject => "[coregui-feedback] New CoreGUI user feedback"
-  default :reply_to => AppSettings.config["mail"]["reply_to"]
+  default :bcc => AppSettings.config["mail"]["to"],
+          :subject => "[coregui-feedback] New CoreGUI user feedback"
   # @param [String] feedbackText
   # @param [String] userEmail
   # @param [String] technicalInfo
   def feedback_email(feedbackText, userEmail, technicalInfo, versionNumber)
     @feedbackData = {:text => feedbackText, :email => userEmail, :techInfo => technicalInfo, :versionNumber => versionNumber}
-    mail(:to => userEmail)
+    mail(:to => "bugs@openphacts.org", :reply_to => userEmail, :from => userEmail)
   end
 end
