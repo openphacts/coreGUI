d2c9e861d3aef1598e5cafa0aea7e261e7c5bc25
Rishi Ramgolam <ramgolam@aqnowledge.com>
2013-04-09 17:19:01 +0100

    An Array of Keywords are returned rather than a string - fix

--- public/app/view/target_by_name/TargetPanel.js
+++ public/app/view/target_by_name/TargetPanel.js
@@ -9,7 +9,7 @@
 Ext.define('LSP.view.target_by_name.TargetPanel', {
     extend:'Ext.panel.Panel',
     alias:'widget.TargetPanel',
-    title:'Target Data',
+    //title:'Target Data',
     anchor:'100% 100%',
     autoScroll:true,
     bodyPadding:'10px',
@@ -274,7 +274,8 @@ Ext.define('LSP.view.target_by_name.TargetPanel', {
         else if (fieldId == 'keywords') {
             if (value != null) {
                 var keywordfield = this.down('#' + fieldId);
-                var keywordValue = value.replace(new RegExp(" ,", 'g'),",");
+                //var keywordValue = value.replace(new RegExp(" ,", 'g'),","); old API value return (not array)
+                var keywordValue = value.join(", ");
                 keywordfield.setValue(keywordValue);
                 keywordfield.show();
             }
